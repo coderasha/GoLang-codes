1082d7f342aceba3c2e9a63b6090bdf078f35225
“coderasha” <ashadipta@gmail.com>
2020-07-31 20:53:34 +0530

    for loop

---
+++ for-loop.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// Main function
+func main() {
+
+	// for loop
+	// This loop starts when i = 0
+	// executes till i<4 condition is true
+	// post statement is i++
+	var q int = 34
+	for i := 1; i < 4; i++ {
+		fmt.Printf("GeeksforGeeks+%d%d \n", i, q)
+	}
+
+}
