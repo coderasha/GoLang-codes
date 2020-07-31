6be4f45b87a4c79a7c499eabbbece8774f9463dc
“coderasha” <ashadipta@gmail.com>
2020-07-31 22:02:46 +0530

    changes

--- break.go
+++ break.go
@@ -4,14 +4,17 @@ import "fmt"
 
 // Main function
 func main() {
-	for i := 0; i < 5; i++ {
+	for i := 0; i < 10; i++ {
 
 		fmt.Println(i)
-
+		if i == 4 {
+			continue
+		}
 		// For loop breaks when the value of i = 3
 		if i == 3 {
 			break
 		}
+
 	}
 
 }
