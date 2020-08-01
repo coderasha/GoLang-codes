a14da8f63a9b032589fa3d9c054434e357295088
“coderasha” <ashadipta@gmail.com>
2020-08-01 20:50:25 +0530

    new code

--- array.go
+++ array.go
@@ -3,15 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var n [10]int /* n is an array of 10 integers */
-	var i int
-
-	/* initialize elements of array n to 0 */
-	for i := 0; i < 10; i++ {
-		n[i] = i + 100 /* set element at location i to i + 100 */
-
-		/* output each array element's value */
-
-		fmt.Printf("Element[%d] = %d\n", i, n[i])
+	number := 70
+	guess := 70
+	if guess < number {
+		fmt.Println("low")
+	}
+	if guess > number {
+		fmt.Println("high")
+	}
+	if guess == number {
+		fmt.Println("same")
 	}
 }
