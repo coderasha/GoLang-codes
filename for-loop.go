61cea825ff48e0a059c999785ef5becd61e27a57
Ashadipta Pati <44902407+coderasha@users.noreply.github.com>
2020-08-05 10:50:04 +0530

    Update for-loop.go

--- for-loop.go
+++ for-loop.go
@@ -9,7 +9,7 @@ func main() {
 	// This loop starts when i = 0
 	// executes till i<4 condition is true
 	// post statement is i++
-        Var r int = 36
+        
 	var q int = 34
 	for i := 1; i < 4; i++ {
 		fmt.Printf("GeeksforGeeks+%d%d \n", i, q)
