be3346e7774f0f0826ba6d3c9fc9296adbd9bd4d
Ashadipta Pati <44902407+coderasha@users.noreply.github.com>
2020-08-13 18:17:19 +0530

    Update array.go

--- array.go
+++ array.go
@@ -1,15 +1,14 @@
-package main
-
-import "fmt"
-
-func main() {
-
-	var x [5]int
-	var i, j int
-	for i = 0; i < 5; i++ {
-		x[i] = i
-	}
-	for j = 0; j < 5; j++ {
-		fmt.Printf("Element[%d] = %d\n", j, x[j])
-	}
-}
+package main  
+import "fmt"  
+func main() {  
+   /* an array with 3 rows and 3 columns*/  
+   var a = [3][3]int{ {1,2,3}, {4,5,6}, {7,8,9}}  
+   var i, j int  
+   /* output each array element's value */  
+   for  i = 0; i < 3; i++ {  
+      for j = 0; j < 3; j++ {  
+         fmt.Print(a[i][j] )  
+      }  
+      fmt.Println()  
+   }  
+}  
