b6b9b73aff992f0d5f6169f0740886258b95b462
“coderasha” <ashadipta@gmail.com>
2020-08-13 20:05:44 +0530

    new

--- array.go
+++ array.go
@@ -3,13 +3,14 @@ package main
 import "fmt"
 
 func main() {
-
-	var x [5]int
+	/* an array with 3 rows and 3 columns*/
+	var a = [3][3]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
 	var i, j int
-	for i = 0; i < 5; i++ {
-		x[i] = i
-	}
-	for j = 0; j < 5; j++ {
-		fmt.Printf("Element[%d] = %d\n", j, x[j])
+	/* output each array element's value */
+	for i = 0; i < 3; i++ {
+		for j = 0; j < 3; j++ {
+			fmt.Print(a[i][j])
+		}
+		fmt.Println()
 	}
 }
